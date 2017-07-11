3f634b9f7203d71a30940b7b3903a5847daae062
Josh Morash <Josh Morash>
2017-07-11 10:18:53 -0300

    adding a read test

--- app/src/androidTest/java/com/acme/a3csci3130/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/ExampleInstrumentedTest.java
@@ -23,31 +23,30 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
 
 /**
- * Instrumentation test, which will execute on an Android device.
+ * Instrumentation tests, which will execute on an Android device.
+ * Thread.sleep() is used because every test other than create will fail
+ * if the emulator is just being loaded for the first time;
  *
+ * @author Joshua Morash
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    String key = "";
     String businessId = "123456789";
     String name = "test";
     String primaryBusiness = "Fisher";
     String address = "test";
     String provinceOrTerritory = "NS";
-    Contact business = new Contact(key, name, businessId, primaryBusiness, address, provinceOrTerritory);
-    DatabaseReference firebaseReference;
-    FirebaseDatabase firebaseDBInstance;
+
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
-    //@Rule
-    //public ActivityTestRule<CreateContactAcitivity> createContactAcitivityActivityTestRule = new ActivityTestRule<CreateContactAcitivity>(CreateContactAcitivity.class);
 
     @Test
     public void test_submitInfoButton() throws Exception {
@@ -76,14 +75,20 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
+    public void test_readContact() throws Exception {
+        Thread.sleep(5000);
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0).perform(click());
+        onView(withId(R.id.name)).check(matches(notNullValue()));
+        onView(withId(R.id.primaryBusiness)).check(matches(notNullValue()));
+        onView(withId(R.id.businessId)).check(matches(notNullValue()));
+    }
+
+    @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
 
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        //private MyApplicationData appState = ((MyApplicationData) getApplicationContext());
-        firebaseDBInstance = FirebaseDatabase.getInstance();
-        firebaseReference = firebaseDBInstance.getReference("contacts");
         assertEquals("com.acme.a3csci3130", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/acme/a3csci3130/ExampleUnitTest.java
+++ app/src/test/java/com/acme/a3csci3130/ExampleUnitTest.java
@@ -19,43 +19,4 @@ public class ExampleUnitTest{
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
-    public DatabaseReference firebaseReference;
-    public FirebaseDatabase firebaseDBInstance;
-
-
-    String key;
-    String businessId = "123456789";
-    String name = "test";
-    String primaryBusiness = "Fisher";
-    String address = "test";
-    String provinceOrTerritory = "NS";
-    Contact business = new Contact(key, name, businessId, primaryBusiness, address, provinceOrTerritory);
-
-    @Test
-    public void test_submitInfoButton() throws Exception {
-        firebaseDBInstance = FirebaseDatabase.getInstance();
-        firebaseReference = firebaseDBInstance.getReference("contacts");
-        business.key = firebaseReference.push().getKey();
-        firebaseReference.child(key).setValue(business);
-
-        //assertEquals(business.key, firebaseReference.child(key).child("key").toString());
-        assertEquals(business.businessId, firebaseReference.child(key).child("businessId").toString());
-        assertEquals(business.name, firebaseReference.child(key).child("name").toString());
-        assertEquals(business.primaryBusiness, firebaseReference.child(key).child("primaryBusiness").toString());
-        assertEquals(business.provinceOrTerritory, firebaseReference.child(key).child("provinceOrTerritory").toString());
-        assertEquals(business.address, firebaseReference.child(key).child("address").toString());
-    }
-
-    @Test
-    public void test_updateContact() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void test_eraseContact() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-
-
 }
